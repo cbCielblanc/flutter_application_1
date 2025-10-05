19ea87f8c1c27bb546ccc17e4b7a4ac817ab2684
cbCielblanc <cielblanc.cb@gmail.com>
2025-10-05 15:22:11 +0200

    Remove duplicated script sources

--- scripts/global/default.dart
+++
@@ -1,8 +0,0 @@
-{
-  "onWorkbookOpen": [
-    {"call": "log", "args": ["Classeur initialisé via OptimaScript Dart."]}
-  ],
-  "onWorkbookClose": [
-    {"call": "log", "args": ["Classeur fermé via OptimaScript Dart."]}
-  ]
-}
--- scripts/pages/feuille_1.dart
+++
@@ -1,8 +0,0 @@
-{
-  "onPageEnter": [
-    {"call": "log", "args": ["Entrée sur la page feuille_1 avec l'API Dart."]}
-  ],
-  "onPageLeave": [
-    {"call": "log", "args": ["Sortie de la page feuille_1 avec l'API Dart."]}
-  ]
-}
