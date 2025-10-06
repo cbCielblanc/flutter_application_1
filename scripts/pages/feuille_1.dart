655d874fc0e91dc7336098d8e3887334697fac6d
Coraillon <valentin.dauzere-peres@optimal-logistics.no>
2025-10-06 08:42:58 +0200

    k

---
+++ scripts/pages/feuille_1.dart
@@ -0,0 +1,17 @@
+import 'package:optimascript/api.dart';
+
+Future<void> onPageEnter(ScriptContext context) async {
+  final sheet = context.api.workbook.activeSheet;
+  sheet?.cellByLabel('A1')?.setValue('Feuille 1 ouverte');
+  await context.callHost(
+    'log',
+    positional: <Object?>['Entrée sur la page feuille_1 avec l\'API Dart Test XX 2.'],
+  );
+}
+
+Future<void> onPageLeave(ScriptContext context) async {
+  await context.callHost(
+    'log',
+    positional: <Object?>['Sortie de la page feuille_1 avec l\'API Dart.'],
+  );
+}
